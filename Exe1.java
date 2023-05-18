5c4fd3c07a98bbff9e58050bf4cb117ab7343a7a
Antoniongola <Antoniongola10@gmail.com>
2023-05-18 09:16:04 +0100

    Dois ficheiros salvos no reposítório

---
+++ Exe1.java
@@ -0,0 +1,11 @@
+package Biblioteca;
+import java.util.Scanner;
+public class Exe1 {
+    int soma, a, b;
+    public static void verificadorIdade(int idade){
+        if(idade >= 18)
+            System.out.println("Maior de idade!");
+        else
+            System.out.println("Menor de idade");
+    }
+}
